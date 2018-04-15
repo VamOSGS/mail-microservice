96e89f875d73c4e9c9ff11211d44266e74af5331
VamOSGS <gegham.samvelyan@hotmail.com>
2018-04-16 00:15:32 +0500

    No-cors mode enebled

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { json, send } = require('micro');
 const { transporter, mailOptions } = require('./src/mail');
+const cors = require('micro-cors')();
 require('dotenv').config();
 
 const { AUTH } = process.env;
-module.exports = async (req, res) => {
+module.exports = cors(async (req, res) => {
   if (req.headers.authorization !== AUTH) {
     return {
       success: false,
@@ -19,4 +20,4 @@ module.exports = async (req, res) => {
     if (err) send(res, 505, { success: false, message: 'Somthing went wrong' });
     else send(res, 200, { success: true, message: 'Mail successfully sent' });
   });
-};
+});
